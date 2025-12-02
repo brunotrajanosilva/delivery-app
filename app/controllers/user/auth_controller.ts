c36a61a021c33fd9710236266b5d8bb0150e28c2
brunotrajanosilva <silva3bruno3@gmail.com>
2025-12-02 18:44:59 -0300

    update: add validator in auth_controller

--- app/controllers/user/auth_controller.ts
+++ app/controllers/user/auth_controller.ts
@@ -1,14 +1,17 @@
 import type { HttpContext } from "@adonisjs/core/http";
 import User from "#models/user/user";
 import hash from "@adonisjs/core/services/hash";
+import { loginValidator } from "#validators/auth";
 
 export default class AuthController {
   async login({ request, response }: HttpContext) {
+    await request.validateUsing(loginValidator);
+
     const { email, password } = request.only(["email", "password"]);
     const user = await User.findBy("email", email);
 
     if (!user) {
-      return response.unauthorized({ message: "Invalid credentials" });
+      return response.unauthorized({ message: "User not found" });
     }
 
     const isPasswordValid = await hash.verify(user.password, password);
