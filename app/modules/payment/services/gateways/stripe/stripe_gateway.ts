ab36e1b4c193ab15e2cfbe55bd1a6879129b11d9
brunotrajanosilva <silva3bruno3@gmail.com>
2026-01-04 17:26:18 -0300

    update stripe_gateway

--- app/modules/payment/services/gateways/stripe/stripe_gateway.ts
+++ app/modules/payment/services/gateways/stripe/stripe_gateway.ts
@@ -1,46 +1,48 @@
-import GatewayAbstract from "../gateway_abstract.js"
-import StripeClient from "./stripe_client.js"
+import GatewayAbstract from "../gateway_abstract.js";
+import StripeClient from "./stripe_client.js";
 
-import Stripe from "stripe"
+import Stripe from "stripe";
 // import {StripeClient} from "./stripe_client"
 
 export default class StripeGateway extends GatewayAbstract {
-  private stripe = StripeClient.getInstance()
+  private stripe = StripeClient.getInstance();
 
-
-  public async createPayment(orderId: number, amount: string, currency: string) {
+  public async createPayment(
+    orderId: number,
+    amount: number,
+    currency: string,
+  ): Promise<Stripe.Response<Stripe.PaymentIntent>> {
     const paymentIntent = await this.stripe.paymentIntents.create({
       amount, // in cents
       currency,
       metadata: {
-        orderId: orderId.toString()
-      }
+        orderId: orderId.toString(),
+      },
       // payment_method_types: ['card'],
-    })
-    return paymentIntent
+    });
+    return paymentIntent;
   }
 
-  public async getPaymentStatus(paymentId: string): Promise<Stripe.Response<Stripe.PaymentIntent>> {
-    const paymentIntent = await this.stripe.paymentIntents.retrieve(paymentId)
-    return paymentIntent
+  public async getPaymentStatus(
+    paymentId: string,
+  ): Promise<Stripe.Response<Stripe.PaymentIntent>> {
+    // return Promise.resolve({});
+    const paymentIntent = await this.stripe.paymentIntents.retrieve(paymentId);
+
+    return paymentIntent;
     /* return {
       id: paymentIntent.id,
       status: paymentIntent.status,
       amount: paymentIntent.amount,
       currency: paymentIntent.currency,
     } */
-
   }
 
   public async refund(paymentId: string) {
     const refund = await this.stripe.refunds.create({
       payment_intent: paymentId,
-    })
+    });
 
-    return refund
+    return refund;
   }
-
-
-
 }
-  
